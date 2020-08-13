fc82060a66a16de7865935e5c0e9b16e8b28036b
Ednar Echeverría <rodak.ro@gmail.com>
2020-08-13 14:16:07 -0400

    CSS updated

--- settings.py
+++ settings.py
@@ -199,7 +199,7 @@ ALLOW_ELECTION_INFO_URL = (get_from_env('ALLOW_ELECTION_INFO_URL', '0') == '1')
 FOOTER_LINKS = json.loads(get_from_env('FOOTER_LINKS', '[]'))
 FOOTER_LOGO_URL = get_from_env('FOOTER_LOGO_URL', None)
 
-WELCOME_MESSAGE = get_from_env('WELCOME_MESSAGE', "This is the default message")
+WELCOME_MESSAGE = get_from_env('WELCOME_MESSAGE', "ELECCIONES FECH 2020")
 
 HELP_EMAIL_ADDRESS = get_from_env('HELP_EMAIL_ADDRESS', 'tricelfech@gmail.com')
 
